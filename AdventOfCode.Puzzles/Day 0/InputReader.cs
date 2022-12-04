a2a41ea90e826d944e1ac7472c8b75779fc79331
D. Gierveld <d.gierveld@outlook.com>
2022-12-04 09:49:24 +0100

    Added a file reader to template.

---
+++ AdventOfCode.Puzzles/Day 0/InputReader.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Puzzles.Day_0
+{
+    internal class InputReader
+    {
+        public void Read(string fileName)
+        {
+            using (var fileStream = File.OpenRead(fileName))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        var line = streamReader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
