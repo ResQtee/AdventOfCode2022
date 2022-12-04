1880d5d289a17f067276defcb70ca40ff2be6d3d
D. Gierveld <d.gierveld@outlook.com>
2022-12-04 10:19:00 +0100

    Added Day0 as part of copy paste template.

---
+++ AdventOfCode2022.CLI/Day0.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Puzzles.Day_4;
+
+namespace AdventOfCode2022.CLI;
+
+public class Day0
+{
+    public static void Print()
+    {
+        Console.WriteLine("----- Day 0 -----");
+        Console.WriteLine("Example 1:");
+        
+        Console.WriteLine($"Answer: ");
+
+        Console.WriteLine();
+        Console.WriteLine("Puzzle 1:");
+
+        Console.WriteLine($"Answer: ");
+    }
+}
