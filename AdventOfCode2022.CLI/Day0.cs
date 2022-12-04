3d826d72516e6d602a5cbb16a6758708ddd407a3
D. Gierveld <d.gierveld@outlook.com>
2022-12-04 10:44:04 +0100

    Minor Day 0 template change

--- AdventOfCode2022.CLI/Day0.cs
+++ AdventOfCode2022.CLI/Day0.cs
@@ -7,13 +7,20 @@ public class Day0
     public static void Print()
     {
         Console.WriteLine("----- Day 0 -----");
+
         Console.WriteLine("Example 1:");
-        
+        Console.WriteLine($"Answer: ");
+
+        Console.WriteLine();
+        Console.WriteLine("Example 2:");
         Console.WriteLine($"Answer: ");
 
         Console.WriteLine();
         Console.WriteLine("Puzzle 1:");
+        Console.WriteLine($"Answer: ");
 
+        Console.WriteLine();
+        Console.WriteLine("Puzzle 2:");
         Console.WriteLine($"Answer: ");
     }
 }
