62ded50601c9d5cc37913783063400a9a692f548
D. Gierveld <d.gierveld@outlook.com>
2022-12-17 12:57:24 +0100

    Day 11 preparations WIP

---
+++ AdventOfCode.Puzzles/Day 11/InputReader.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Puzzles.Day_11
+{
+    public class InputReader
+    {
+        public void Read(string fileName)
+        {
+            using (var fileStream = File.OpenRead(fileName))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        var line = streamReader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode.Puzzles/Day 11/Monkey.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Puzzles.Day_11;
+
+public class Monkey
+{
+    /*
+        Starting items: 79, 60, 97
+        Operation: new = old * old
+        Test: divisible by 13
+        If true: throw to monkey 1
+        If false: throw to monkey 3
+    */
+
+    public List<int> WorryLevels { get; set; } = new List<int>();
+
+    public IOperation? Operation { get; set; }
+
+    public bool Divisible { get; set; }
+}
+
+public interface IOperation
+{
+    void Execute("old * old");
+}
